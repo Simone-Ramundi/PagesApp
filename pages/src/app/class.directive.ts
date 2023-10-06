a849171ad14baf41a0427fdd6efe755eef16c7ec
Simone <simoneramundi@outlook.com>
2023-10-06 11:09:04 +0200

    Replacing NgClass with a customized class (AppClass).

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -6,7 +6,14 @@ import { Directive, ElementRef, Input} from '@angular/core';
 export class ClassDirective {
   constructor(private element:ElementRef) {}
 
-  @Input() set backgroundColor(color:string){
-    this.element.nativeElement.style.backgroundColor = color;
+  @Input('appClass') set classNames(classObj:any){
+    // this.element.nativeElement.style.backgroundColor = color;
+    for (let key in classObj){
+      if (classObj[key]){
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
